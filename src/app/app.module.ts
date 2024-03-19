67153d07a77f98df4e83fca49db04875841de919
rebecaaras <rebecaaraujo2013@gmail.com>
2024-03-18 21:06:27 -0300

    Generating page components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { PageTitleComponent } from './page-title/page-title.component';
+import { BigCardComponent } from './big-card/big-card.component';
+import { SmallCardComponent } from './small-card/small-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    PageTitleComponent,
+    BigCardComponent,
+    SmallCardComponent
   ],
   imports: [
     BrowserModule,
